db3e20b93c2c25d8f71ee92469efc30309987fba
Burton Parker <burtonparker@gmail.com>
2021-12-23 11:06:37 -0600

    header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 
 class Main extends Component {
@@ -22,6 +23,7 @@ onCampsiteSelect(campsiteId) {
                 <Header />
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+                <Footer />
             </div>
         );
     };
