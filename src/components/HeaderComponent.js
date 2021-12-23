afbb9a66d573ad23f777a8eb4176f96bb5796f5f
Burton Parker <burtonparker@gmail.com>
2021-12-23 11:00:33 -0600

    resolving dependencies

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,11 +7,12 @@ class Header extends Component {
             <React.Fragment>
                 <Jumbotron fluid>
                     <div className="container">
-                    <div className="row">
-                    <div className="col">
-                    <h1>NuCamp</h1>
-                    <h2>a better way to camp</h2>
-                    </div>
+                        <div className="row">
+                            <div className="col">
+                                <h1>NuCamp</h1>
+                                <h2>a better way to camp</h2>
+                            </div>
+                        </div>
                     </div>
                 </Jumbotron>
                 <Navbar dark sticky="top">
