45272a572bebd7068188ec4d0705f472e52f8d3a
Burton Parker <burtonparker@gmail.com>
2021-12-28 16:54:11 -0600

    end of week 3 assignment

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,33 +2,35 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+function RenderPartner({partner}) {
+    if (partner) {
+        return (
+            <React.Fragment>
+                <Media object src={partner.image} alt={partner.name} width="150" />
+                <Media body className="ml-5 mb-4">
+                    <Media heading>
+                        {partner.name}
+                    </Media>
+                    {partner.description}
+                </Media>
+            </React.Fragment>
+        );
+    }
+    return (
+        <div></div>
+    );
+}
 
 function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            //<h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id} >
+                <RenderPartner partner={partner} />
+            </Media>
         );
     });
-
-    const RenderPartner = ({partner}) => {
-        if (partner) {
-            return (
-                <React.Fragment>
-                    <Media object src={partner.image} alt={partner.name} width="150" />
-                    <Media body className="ml-5 mb-4">
-                        <Media heading>
-                            {partner.name}
-                        </Media>
-                        {partner.description}
-                    </Media>
-                </React.Fragment>
-            );
-        }
-        return (
-            <div></div>
-        );
-    }
 
     return (
         <div className="container">
